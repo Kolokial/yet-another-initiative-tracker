1499f62a85127dcb4f33836d86b3299cf08e08b7
koloqial <koloqial@icloud.com>
2024-06-15 23:07:21 +0100

    Added state managment for tracker and handled auto updating roll.

--- app/src/app/initiative-tracker/initiative-tracker.component.ts
+++ app/src/app/initiative-tracker/initiative-tracker.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
@@ -7,8 +7,10 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MessagingService } from '../messaging.service';
 import { FormsModule } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { Subject, Subscription, debounceTime, distinctUntilChanged } from 'rxjs';
+import { Subject, Subscription, debounceTime, distinctUntilChanged, tap } from 'rxjs';
+import { InitiativeTrackerStoreService } from './initiative-tracker.store.service';
 
 @Component({
   selector: 'initiative-tracker',
@@ -30,42 +32,67 @@ export class InitiativeTrackerComponent {
   @Input() public players: string[] = [];
   @Input() public playerName: string = '';
 
-  private _dexterityModifier: number = 0;
   public get dexterityModifier(): number {
-    return this._dexterityModifier;
+    return this.dataStore.dexterityModifier;
   }
 
   public set dexterityModifier(value: string) {
-    this._dexterityModifier = parseInt(value);
+    this.dataStore.dexterityModifier = parseInt(value);
   }
-  public alertFeat: boolean = false;
 
-  @ViewChild('initiative') initiative!: ElementRef;
+  public get alertFeat(): boolean {
+    return this.dataStore.alertFeat;
+  }
+
+  public set alertFeat(value: boolean) {
+    this.dataStore.alertFeat = value;
+  }
+
   private _isInitiativeInputDisabled: boolean = false;
   public get isInitiativeInputDisabled(): boolean {
     return this._isInitiativeInputDisabled;
   }
-  public initiativeValue!: number;
+
+  public get initiativeValue(): number {
+    return this.dataStore.initiativeValue;
+  }
+  public set initiativeValue(v: number) {
+    this.dataStore.initiativeValue = v;
+  }
 
   private keyup$: Subject<number> = new Subject<number>();
   private keyupSubscription!: Subscription;
 
-  constructor(private messagingService: MessagingService) {}
+  private lastSentRoll: number = 0;
+  private impendingRoll: number = 0;
+
+  constructor(
+    private messagingService: MessagingService,
+    private dataStore: InitiativeTrackerStoreService
+  ) {}
 
   ngOnInit() {
     this.keyupSubscription = this.keyup$
-      .pipe(debounceTime(1000), distinctUntilChanged())
+      .pipe(
+        tap((initiative) => {
+          this.impendingRoll = initiative;
+        }),
+        debounceTime(1000),
+        distinctUntilChanged()
+      )
       .subscribe((initiative) => {
         this.sendInitiative(initiative);
       });
   }
 
   ngOnDestroy() {
+    if (this.lastSentRoll !== this.impendingRoll) {
+      this.sendInitiative(this.impendingRoll);
+    }
     this.keyupSubscription.unsubscribe();
   }
 
   ngAfterViewInit() {
-    console.log(this.initiative);
     this._isInitiativeInputDisabled = false;
   }
 
