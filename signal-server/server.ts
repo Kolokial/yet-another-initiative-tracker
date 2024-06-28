311e20dabd275a742ed0dfd44fd80350646c65b3
koloqial <koloqial@icloud.com>
2024-06-28 16:55:25 +0100

    Changes

--- signal-server/server.ts
+++ signal-server/server.ts
@@ -1,77 +1,76 @@
-const http = require('http');
-const socketIo = require('socket.io');
+const http = require("http");
+const socketIo = require("socket.io");
 
 const server = http.createServer();
 const io = socketIo(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
 });
 
 const rooms = {};
 
-io.on('connection', (socket) => {
-    console.log('New client connected', socket.id);
+io.on("connection", (socket: any) => {
+  console.log("New client connected", socket.id);
 
-    socket.on('joinRoom', (roomId) => {
-        if(!roomId){
-          return
-        }
-        socket.join(roomId);
-        console.log(roomId);
-        if (!rooms[roomId]) {
-            rooms[roomId] = [];
-        }
-        rooms[roomId].push(socket.id);
-        console.log(`Client ${socket.id} joined room ${roomId}`);
-        notifyPeersInRoom(socket, roomId, 'newPeerJoined', socket.id);
+  socket.on("joinRoom", (roomId) => {
+    if (!roomId) {
+      return;
+    }
+    socket.join(roomId);
+    console.log(roomId);
+    if (!rooms[roomId]) {
+      rooms[roomId] = [];
+    }
+    rooms[roomId].push(socket.id);
+    console.log(`Client ${socket.id} joined room ${roomId}`);
+    notifyPeersInRoom(socket, roomId, "newPeerJoined", socket.id);
 
-        io.to(socket.id).emit('roomJoined', rooms[roomId]);
-    });
+    io.to(socket.id).emit("roomJoined", rooms[roomId]);
+  });
 
-    socket.on('offer', (data) => {
-      //console.log(`Offer: ${JSON.stringify(data)}`);
-        socket.to(data.roomId).emit('offer', data);
-    });
+  socket.on("offer", (data) => {
+    //console.log(`Offer: ${JSON.stringify(data)}`);
+    socket.to(data.roomId).emit("offer", data);
+  });
 
-    socket.on('answer', (data) => {
-      //console.log(`Answer: ${JSON.stringify(data)}`);
-        socket.to(data.roomId).emit('answer', data);
-    });
+  socket.on("answer", (data) => {
+    //console.log(`Answer: ${JSON.stringify(data)}`);
+    socket.to(data.roomId).emit("answer", data);
+  });
 
-    socket.on('candidate', (data) => {
-      //console.log(`Candidate: ${JSON.stringify(data)}`);
-        socket.to(data.roomId).emit('candidate', data);
-    });
+  socket.on("candidate", (data) => {
+    //console.log(`Candidate: ${JSON.stringify(data)}`);
+    socket.to(data.roomId).emit("candidate", data);
+  });
 
-    socket.on('disconnect', () => {
-        console.log('Client disconnected', socket.id);
-        removePeerFromRooms(socket.id);
-        
-    });
+  socket.on("disconnect", () => {
+    console.log("Client disconnected", socket.id);
+    removePeerFromRooms(socket.id);
+  });
 });
 
 function notifyPeersInRoom(socket, roomId, event, data) {
-    const peersInRoom = rooms[roomId] || [];
-    console.log(`Notifying peers`);
-    for (const peerId of peersInRoom) {
-        if (peerId !== socket.id) {
-          console.log(`PeerId ${peerId}, SocketId: ${socket.id}`);
-            //io.to(peerId).emit(event, data);
-        }
+  const peersInRoom = rooms[roomId] || [];
+  console.log(`Notifying peers`);
+  for (const peerId of peersInRoom) {
+    if (peerId !== socket.id) {
+      console.log(`PeerId ${peerId}, SocketId: ${socket.id}`);
+      //io.to(peerId).emit(event, data);
     }
+  }
 }
 
 function removePeerFromRooms(peerId) {
-    for (const roomId in rooms) {
-        if (rooms[roomId].includes(peerId)) {
-            rooms[roomId] = rooms[roomId].filter(id => id !== peerId);
-            if (rooms[roomId].length === 0) {
-                delete rooms[roomId];
-            }
-        }
+  for (const roomId in rooms) {
+    if (rooms[roomId].includes(peerId)) {
+      rooms[roomId] = rooms[roomId].filter((id) => id !== peerId);
+      if (rooms[roomId].length === 0) {
+        delete rooms[roomId];
+      }
     }
+  }
 }
 
 const PORT = process.env.PORT || 3000;
