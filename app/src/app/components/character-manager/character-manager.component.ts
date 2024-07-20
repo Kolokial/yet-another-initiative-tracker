53165b0a3b7aab936d3354fa8487a3b34c560c2f
koloqial <koloqial@icloud.com>
2024-07-20 01:34:13 +0100

    Added character manager component

---
+++ app/src/app/components/character-manager/character-manager.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'character-manager',
+  standalone: true,
+  imports: [],
+  templateUrl: './character-manager.component.html',
+  styleUrl: './character-manager.component.scss',
+})
+export class CharacterManagerComponent {
+  /* TODO: 
+    Set character name
+    set character profile picture
+    set character musical cue?
+  */
+}
