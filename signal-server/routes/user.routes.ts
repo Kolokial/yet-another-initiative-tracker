ec9804733b1ba972d6f013ec337657e8434deefd
koloqial <koloqial@icloud.com>
2024-08-03 00:16:28 +0100

    Using upsert method for User.

--- signal-server/routes/user.routes.ts
+++ signal-server/routes/user.routes.ts
@@ -23,19 +23,21 @@ export function setupUserRoutes(
    * 200:
    * description: Successful response
    */
-  app.get("/api/user/:id", checkJwt, (req: Request, res: Response) => {
+  app.get("/api/user", checkJwt, (req: Request, res: Response) => {
     console.log(`Header: ${req.headers.authorization}`);
     res.send(`{"good": "job"}`);
   });
   app.post("/api/user", checkJwt, (req: Request, res: Response) => {
     console.log(`req.body = ` + JSON.stringify(req.body));
-    database.insertUserData(req.body.auth0Id, req.body.displayName);
+    console.log(`Header: ${req.auth?.payload.sub}`);
+    /* TODO: Can remove the auth0Id from body and just use req.auth.payload.sub */
+    database.upsertUserData(req.body.auth0Id, req.body.displayName);
     /* TODO: send back userId */
     res.send("null");
     res.status(201).end();
   });
-  app.patch("/api/user/:id", checkJwt, (req: Request, res: Response) => {
-    database.updateUserData(req.params.id, req.body);
+  app.patch("/api/user/", checkJwt, (req: Request, res: Response) => {
+    database.upsertUserData(req.headers.authorization as string, req.body);
     res.status(200).end();
   });
   app.get(`/api/user`, checkJwt, (req, res) => {
