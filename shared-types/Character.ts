dc21e54ccf931cb95b494e296d017b684c9cecbc
koloqial <koloqial@icloud.com>
2024-08-04 00:30:07 +0100

    Created shared types for API and UI to use.

---
+++ shared-types/Character.ts
@@ -0,0 +1,15 @@
+export type PlayerCharacter = {
+  PlayerCharacterId: number;
+  UserId: number;
+  CharacterName: string;
+  DexterityMod: number;
+  LuckStone: boolean;
+  AlertFeat: boolean;
+  IsDeleted: boolean;
+};
+
+export type PlayerCharacterDiceRoll = {
+  PlayerCharacterId: number;
+  RoomId: string;
+  DiceRoll: number;
+};
---
+++ shared-types/User.ts
@@ -0,0 +1,4 @@
+export type User = {
+  DisplayName: string;
+  Auth0Id: string;
+};
