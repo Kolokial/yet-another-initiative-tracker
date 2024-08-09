e3d6c5a33b52a7b6d055d7e2fff9582b182ca386
koloqial <koloqial@icloud.com>
2024-08-09 21:21:17 +0100

    Abstracting soon-to-be common methods.

---
+++ app/src/app/shared-services/BaseApi.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '@auth0/auth0-angular';
+import { mergeMap, Observable } from 'rxjs';
+import { API_FULL_URL } from '../constants';
+
+export abstract class BaseApi {
+  constructor(
+    protected http: HttpClient,
+    protected auth: AuthService
+  ) {
+    this.http.head;
+  }
+
+  protected getRequest<T>(url: string): Observable<T> {
+    return this.auth.getAccessTokenSilently().pipe<T>(
+      mergeMap((token: string) => {
+        return this.http.get<T>(`${API_FULL_URL}${url}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+      })
+    );
+  }
+
+  protected postRequest<T, R>(url: string, body: T): Observable<R> {
+    return this.auth.getAccessTokenSilently().pipe(
+      mergeMap((token: string) => {
+        return this.http.post<R>(`${API_FULL_URL}${url}`, body, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+        });
+      })
+    );
+  }
+
+  protected patchRequest<T, P>(url: string, body: T): Observable<P> {
+    return this.auth.getAccessTokenSilently().pipe(
+      mergeMap((token: string) => {
+        return this.http.patch<P>(`${API_FULL_URL}${url}`, body, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+        });
+      })
+    );
+  }
+}
---
+++ app/src/app/shared-services/user-api.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AuthService, User as AuthUser } from '@auth0/auth0-angular';
+import { Observable, mergeMap } from 'rxjs';
+import { User } from '@shared-types/User';
+import { BaseApi } from './BaseApi';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserApiService extends BaseApi {
+  constructor(http: HttpClient, auth: AuthService) {
+    super(http, auth);
+  }
+
+  public canActivate(): Observable<boolean> {
+    /* TODO: need to move this out of the service */
+    return this.auth.isAuthenticated$;
+  }
+
+  public createUser(auth0Id: string, displayName?: string) {
+    this.postRequest(
+      `/api/user`,
+      `{"auth0Id": "${auth0Id}", "displayName": "${displayName}"}`
+    ).subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+    });
+  }
+
+  public updateUserDisplayName(displayName: string) {
+    this.auth.user$
+      .pipe(
+        mergeMap((user: AuthUser | null | undefined) => {
+          return this.postRequest(`/api/user`, `{"DisplayName":"${displayName}"}`);
+        })
+      )
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+        },
+      });
+  }
+
+  public getUser(): Observable<User> {
+    return this.getRequest<User>(`/api/user`);
+  }
+}
