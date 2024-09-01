bb65b8b0a6cd77be3a81e18e6a4972092525b86f
koloqial <koloqial@icloud.com>
2024-09-02 00:14:51 +0100

    DataChannels types

---
+++ app/src/app/types/dataChannels.ts
@@ -0,0 +1,36 @@
+import { Observable } from 'rxjs';
+import { Envelope, ProfileUpdateMessage, DiceRollMessage, PeerId } from './messages';
+
+export type DataChannelCollection = {
+  PeerId: string;
+  ProfileChannel: DataChannelEventsTraffic<ProfileUpdateMessage>;
+  DiceChannel: DataChannelEventsTraffic<DiceRollMessage>;
+};
+
+export type DataChannelEventsTraffic<T> = {
+  InboundEvents: DataChannelInboundEvents<T>;
+  OutboundEvents: DataChannelOutboundEvents<T>;
+};
+
+export type DataChannelInboundEvents<T> = {
+  onOpen: Observable<Event>;
+  onMessage: Observable<Envelope<T>>;
+  onClosing: Observable<Event>;
+  onClose: Observable<PeerId>;
+  onError: Observable<Event>;
+};
+
+export type DataChannelOutboundEvents<T> = {
+  sendMessage: (message: Envelope<T>) => void;
+};
+
+export type RTCDataChannelCollection = {
+  ProfileChannel: RTCDataChannel | null;
+  DiceChannel: RTCDataChannel | null;
+};
+
+export type YAITCustomOffer = {
+  offer: RTCSessionDescriptionInit;
+  roomId: string;
+  peerId: string;
+};
