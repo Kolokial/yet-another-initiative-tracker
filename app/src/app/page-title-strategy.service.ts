6aef60b6f8d64d961d8512c7e190b27bf5026b60
koloqial <koloqial@icloud.com>
2025-02-03 22:09:42 +0000

    Removing console.logs

--- app/src/app/page-title-strategy.service.ts
+++ app/src/app/page-title-strategy.service.ts
@@ -6,11 +6,9 @@ import { RouterStateSnapshot, TitleStrategy } from '@angular/router';
 export class PageTitleStrategy extends TitleStrategy {
   constructor(private readonly title: Title) {
     super();
-    console.log('running pagetitle');
   }
   override updateTitle(routerState: RouterStateSnapshot) {
     const title = this.buildTitle(routerState);
-    console.log('running pagetitle', title);
     if (title !== undefined) {
       this.title.setTitle(`${title} | YAIT | Yet Another Initiative Tracker`);
     }
